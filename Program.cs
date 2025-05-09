52b46b8e8aee302a46027707c2603b238e09bae3
Raul Arroyo <rarroyo@gdeiman.com>
2025-05-09 14:50:41 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
+
+// Agrega servicios de controladores con soporte para JSON
 builder.Services.AddControllers().AddNewtonsoftJson();
+
 var app = builder.Build();
+
 app.UseHttpsRedirection();
-app.MapControllers();
+
+app.MapControllers(); // Asegura que los controladores se mapeen
+
 app.Run();
