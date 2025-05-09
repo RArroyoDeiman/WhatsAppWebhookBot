a33714dcce61e4743179ff56c0c75fba10fa4317
Raul Arroyo <rarroyo@gdeiman.com>
2025-05-09 10:59:07 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.MapControllers();
